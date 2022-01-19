01b52c2341d148499f799d45dff4b703c8d8ef97
Mayank <goyalmayank522@gmail.com>
2022-01-19 23:56:36 +0530

    devise gem used

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+    post '/users/edit' => 'devise/registrations#edit'
+  end
+  resources :articles
+  root "sessions#welcome"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
